82c4cf5b08bb4b4fda6f7ab997516951e81f236b
tarik <tarik@gmai.com>
2024-12-05 23:21:49 +0600

    faq done

---
+++ asstes/js/custom.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    // item1
+    $('#faq_title1').click(function(){
+        $('#faq_add_cls1').slideToggle(300);
+    });
+
+    // item2
+    $('#faq_title2').click(function(){
+        $('#faq_add_cls2').slideToggle(300);
+    });
+
+    // item3
+    $('#faq_title3').click(function(){
+        $('#faq_add_cls3').slideToggle(300);
+    });
+
+});
